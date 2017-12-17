0c5b098a61b9fd9f76043eec5db88d614a52025f
saxtheowl <saxtheowl@gmail.com>
2017-12-17 22:52:41 +0100

    ok

--- src/score.c
+++ src/score.c
@@ -7,14 +7,13 @@
 
 int		get_multiplier(t_game *game, char symbol)
 {
-  printf("symbol multiplier =%c\n", symbol);
   if(symbol >= game->symbol_max_letter && symbol <= game->symbol_max_word)
     {
-      printf("to return get multiplier =%d\n", ((symbol - '0') - (game->symbol_max_letter - '0')));
+      printf("multiplier =%d\n", (symbol - '0') - (game->symbol_max_letter - '0'));
       return((symbol - '0') - (game->symbol_max_letter - '0'));
     }
-  else
-    return(0);
+  printf("get multiplier return 0\n");
+  return(0);
 }
 
 int		get_score_from_letter(t_game *game, char c, char symbol, bool is_new_word)
@@ -23,19 +22,17 @@ int		get_score_from_letter(t_game *game, char c, char symbol, bool is_new_word)
   int		point = game->letters_point[i];
   int		tmp;
 
-  printf("i =%d\n", i);
-  printf("point =%d\n", point);
   if(is_new_word == true)// wrong to fix
     {
-      printf("point2 =%d\n", point);
+      printf("1get score from letter to return =%d\n", point);
       return(point);
     }
   else if(symbol > '0' && symbol <= game->symbol_max_letter)
     {
-      printf("to return score from letter =%d\n", (point * ((symbol - '0') + 1)));
-      printf("symbol == %d\n", ((symbol - '0') + 1));
+      printf("2get score from letter to return =%d\n", (point * ((symbol - '0') + 1)));
       return(point * ((symbol - '0') + 1));
     }
+  printf("3get score from letter to return =%d\n", point);
   return(point);
 }
 
@@ -45,31 +42,26 @@ int		get_special_point(t_game *game, int score, int special_pos)
   int		i = get_number_from_letter(game->word_test[special_pos]);
   int		point = game->letters_point[i];
 
-  printf("special char start\n");
-  printf("special_pos =%d\n", special_pos);
-  printf("i =%d\n", i);
-  printf("special new letter =%c\n", game->word_test[special_pos]);
-  printf("special new symbol =%c\n", game->road_word[special_pos]);
-  printf("letter point =%d\n", point);
   if(game->road_word[special_pos] > '0' && game->road_word[special_pos] <= game->symbol_max_letter)
     {
-      
-      printf("special multiplier =%d\n", multiplier);
-      printf("special char char point to return =%d\n", (point * ((game->road_word[special_pos] - '0'))));
-      return((point * ((game->road_word[special_pos] - '0') + 1)));
+      printf("special point letter=%d\n", ((point * ((game->road_word[special_pos] - '0') + 1))));
+      return((point * (game->road_word[special_pos] - '0')));
     }
   else if(game->road_word[special_pos] >= game->symbol_max_letter && game->road_word[special_pos] <= game->symbol_max_word)
       {
-	printf("special score multiply word to return=%d\n", score * ((game->road_word[special_pos] - '0') - (game->symbol_max_letter)));
+	printf("special point word=%d\n", (score * ((game->road_word[special_pos] - '0') - (game->symbol_max_letter))));
 	return(score * ((game->road_word[special_pos] - '0') - (game->symbol_max_letter)));
       }
-  printf("wut error special point\n");
+  return(0);
 }
 
 bool		get_bingo(char *word)
 {
   if(strlen(word) == 7)
-    return(true);
+    {
+      printf("BINGO !\n");
+      return(true);
+    }
     return(false);
 }
 
@@ -78,19 +70,24 @@ int		get_score(t_game *game, char *word, bool is_new_word, int special_pos)
   int		multiplier = 1;
   int		score = 0;
 
-  printf("word size =%d\n", strlen(word));
+  printf("word to score = %s\n", word);
   for(int i = 0; word[i] != '\0'; i++)
     {
       score = get_score_from_letter(game, word[i], game->road_word[i], is_new_word) + score;
-      printf("score before multiplier = %d\n", score);
+      printf("0score to return =%d\n", score);
       if(is_new_word == false)
 	multiplier = get_multiplier(game, game->road_word[i]) + multiplier;
     }
-  if(is_new_word == 0)
+  if(is_new_word == false)
     score = score * multiplier;
   else
-    score = score + get_special_point(game, score, special_pos);
+    {
+      score = score + get_special_point(game, score, special_pos);
+      printf("1score to return =%d\n", score);
+    }
   if(get_bingo(word) == true)
     return(score + 50);
+  printf("multiplier = %d\n", multiplier);
+  printf("2score to return =%d\n", score);
   return(score);
 }
